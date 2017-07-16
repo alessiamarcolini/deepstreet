8f55183aba6778f9ad84dd21159ec8631756a3ea
alessiamarcolini <98marcolini@gmail.com>
2017-07-16 12:34:15 +0200

    Added count images per class util

---
+++ utils/count_imageperclass_util.py
@@ -0,0 +1,17 @@
+import os
+
+images = os.listdir("dataset/train/")
+
+classes = {}
+
+for i in images:
+    isplit = i.split("_")
+    cl = isplit[0]
+    if cl in classes:
+        classes[cl] += 1
+    else:
+        classes[cl] = 1
+
+
+for c in sorted(classes):
+    print(c,classes[c])
